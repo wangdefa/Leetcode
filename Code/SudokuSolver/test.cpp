5f9a5a4bec5d96db7ea8f3a0ad56fbf47d4ef0a3
Defa Wang <190870210@qq.com>
2018-05-01 22:21:52 +0800

    Track 4 files into repository.
    - untracked Code/CombinationSum/CombinationSum.h
    - untracked Code/CombinationSum/test.cpp
    - untracked Code/SudokuSolver/solveSudoku.h
    - untracked Code/SudokuSolver/test.cpp
    
    Auto commit by GitBook Editor

---
+++ Code/SudokuSolver/test.cpp
@@ -0,0 +1,24 @@
+#include "solveSudoku.h"
+
+void main(){
+	// input 
+	char src[SIZE][SIZE] = {
+	{ '5', '3', '.', '.', '7', '.', '.', '.', '.' },
+	{ '6', '.', '.', '1', '9', '5', '.', '.', '.' },
+	{ '.', '9', '8', '.', '.', '.', '.', '6', '.' },
+	{ '8', '.', '.', '.', '6', '.', '.', '.', '3' },
+	{ '4', '.', '.', '8', '.', '3', '.', '.', '1' },
+	{ '7', '.', '.', '.', '2', '.', '.', '.', '6' },
+	{ '.', '6', '.', '.', '.', '.', '2', '8', '.' },
+	{ '.', '.', '.', '4', '1', '9', '.', '.', '5' },
+	{ '.', '.', '.', '.', '8', '.', '.', '7', '9' }};
+	Solution solution;
+	for (uint8_t i = 0; i < SIZE; i++){
+		solution.board.push_back(vector<char>(src[i], src[i] + SIZE));
+	}
+
+	// processing and display
+	solution.display(true); // display the input
+	solution.solveSudoku();
+	solution.display(false); // display the input
+}
